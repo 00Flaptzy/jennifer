9dd4da294734c32d71d01f95231c6fbebaffd0b2
Black-EDG <yhurycristiamanampaquispe@gmail.com>
2026-01-11 18:58:25 -0500

    Add root and health endpoints

---
+++ src/main/java/com/epiis/app/controller/HealthController.java
@@ -0,0 +1,19 @@
+package com.epiis.app.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend Polaris API is running 🚀";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
